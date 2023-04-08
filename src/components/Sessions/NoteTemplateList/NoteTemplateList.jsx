8783e659cf1552fb6b00c59b096821a0828dcdd2
Johannsephil <jay3pee@gmail.com>
2023-04-08 18:27:13 +0100

    add note templates list component

---
+++ src/components/Sessions/NoteTemplateList/NoteTemplateList.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "./NoteTemplateList.module.scss";
+
+const NoteTemplateList = () => {
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.templates}>
+				<h3 className={`heading__small ${styles.heading}`}>
+					Browse Templates
+				</h3>
+				<div className={styles.template}>
+					<h3 className={styles.template_heading}>General Meeting</h3>
+					<p className={styles.template_desc}>
+						Are you meeting with staff of your company? Try this
+						template
+					</p>
+				</div>
+				<div className={styles.template}>
+					<h3 className={styles.template_heading}>General Meeting</h3>
+					<p className={styles.template_desc}>
+						Are you meeting with staff of your company? Try this
+						template
+					</p>
+				</div>
+				<div className={styles.template}>
+					<h3 className={styles.template_heading}>General Meeting</h3>
+					<p className={styles.template_desc}>
+						Are you meeting with staff of your company? Try this
+						template
+					</p>
+				</div>
+				<div className={styles.template}>
+					<h3 className={styles.template_heading}>General Meeting</h3>
+					<p className={styles.template_desc}>
+						Are you meeting with staff of your company? Try this
+						template
+					</p>
+				</div>
+				<div className={styles.template}>
+					<h3 className={styles.template_heading}>General Meeting</h3>
+					<p className={styles.template_desc}>
+						Are you meeting with staff of your company? Try this
+						template
+					</p>
+				</div>
+				<div className={styles.template}>
+					<h3 className={styles.template_heading}>General Meeting</h3>
+					<p className={styles.template_desc}>
+						Are you meeting with staff of your company? Try this
+						template
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NoteTemplateList;
